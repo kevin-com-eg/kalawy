be01daff97743aceac4d7750386cf943494eede8
kevin-com-eg <k24827753@gmail.com>
2026-08-24 23:47:32 +0300

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,9 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+
+const SUPABASE_URL = "https://kiiizfwjccaywiugsspx.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_HhSUrNpQBBnYX1umIM7mvQ_d-RZs_Rp";
+
+export const supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
